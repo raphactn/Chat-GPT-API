289b985420f74f6afbf7668f232ca46889b3b0eb
raphactn <raphael@leeksolucoes.com.br>
2023-01-14 22:09:00 -0300

    ajustes na requisição

--- services/OpenIa.ts
+++ services/OpenIa.ts
@@ -8,12 +8,11 @@ const OpenIA = async ({ data }: any) => {
   const response = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: data,
-    temperature: 0.5,
+    temperature: 0,
     max_tokens: 60,
     top_p: 1.0,
-    frequency_penalty: 0.5,
+    frequency_penalty: 0.0,
     presence_penalty: 0.0,
-    stop: ["You:"],
   });
 
   return response;
